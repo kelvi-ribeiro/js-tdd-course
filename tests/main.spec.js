304018f18143ae2c9034e4c6c30ad2a4339858c2
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-10 02:33:11 -0300

    Aula 76. Funcionamento do Mocha - descbribe, Context, it...

---
+++ tests/main.spec.js
@@ -0,0 +1,19 @@
+describe('Main', function () {
+  describe('Method A', function () {
+    context('Case 1', function(){
+      it('schould happen blabla',function(){
+        // Espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar (4) => true | false => broken test
+
+      });
+    });
+    context('Case 2', function(){
+
+
+    });
+  });
+  describe('Mehotd B', function() {
+
+  });
+}); 
