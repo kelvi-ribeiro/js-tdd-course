973de2034e0dbf2cfccf3c170ac6329095b0c546
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-10 02:39:47 -0300

    Aula 77. Reporters do Mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,19 +1,31 @@
 describe('Main', function () {
   describe('Method A', function () {
-    context('Case 1', function(){
-      it('schould happen blabla',function(){
+    context('Case 1', function () {
+      it('schould happen blabla', function () {
         // Espera que aconteça
         // Entrada de dados / método sum(2,2)
         // Espera retornar (4) => true | false => broken test
 
       });
     });
-    context('Case 2', function(){
+    context('Case 2', function () {
+      it('schould happen blabla', function () {
+        // Espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar (4) => true | false => broken test
+      
+      });
 
+      it('schould happen mimi', function () {
+        // Espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar (4) => true | false => broken test
+
+      });
 
     });
   });
-  describe('Mehotd B', function() {
+  describe('Mehotd B', function () {
 
   });
 }); 
