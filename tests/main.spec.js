c7b1d6c28b56079163cd60fa3bb166cb42ecfd36
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-07-13 18:16:08 -0300

    Aula 79. Introducao aos Hooks do Mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,32 +1,29 @@
 describe('Main', function () {
-  describe('Method A', function () {
-    context('Case 1', function () {
-      it.skip('schould happen blabla', function () {
-        // Espera que aconteça
-        // Entrada de dados / método sum(2,2)
-        // Espera retornar (4) => true | false => broken test
-        throw new Error('just an error');
-      });
+  //Roda uma vez, antes do bloco
+    before(function () {
+      console.log('before')
+  });
+  //Roda uma vez, depois do bloco
+    after(function () {
+      console.log('after');
+  });
+    // Roda todas as vezes, antes de CADA bloco
+    beforeEach(function () {
+      console.log('beforeEach');
+  });
+    // Roda todas as vezes, depois de CADA bloco
+    afterEach(function () {
+      console.log('afterEach');
     });
-    context('Case 2', function () {
-      it('schould happen blabla', function () {
-        // Espera que aconteça
-        // Entrada de dados / método sum(2,2)
-        // Espera retornar (4) => true | false => broken test
-        throw new Error('just an error');
-      
-      });
 
-      it('schould happen mimi', function () {
-        // Espera que aconteça
-        // Entrada de dados / método sum(2,2)
-        // Espera retornar (4) => true | false => broken test
-
-      });
+    it('test 1',function(){
 
     });
-  });
-  describe('Mehotd B', function () {
 
-  });
+    it('test 2',function(){
+
+    });
+
+    
+
 }); 
