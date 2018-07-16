c552871e776c40147c5a57c157bbffedad940388
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-07-16 11:48:10 -0300

    Aula 80. Hooks na Pratica

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,27 +1,39 @@
 describe('Main', function () {
+  var arr; 
   //Roda uma vez, antes do bloco
     before(function () {
-      console.log('before')
+      // Pode ser usado para inicar uma conexão no banco
+      // Pode ser usado para criar um conjunto de dados
   });
   //Roda uma vez, depois do bloco
     after(function () {
-      console.log('after');
+      // Pode ser usado para fechar uma conexão no banco
+      // Pode ser usado para apagar um conjunto de dados criado no after
   });
     // Roda todas as vezes, antes de CADA bloco
     beforeEach(function () {
-      console.log('beforeEach');
+       arr = [1, 2, 3];
   });
     // Roda todas as vezes, depois de CADA bloco
     afterEach(function () {
-      console.log('afterEach');
+      
+    });       
+
+    it('should have a size of 4 when push another value to the array',function(){
+      
+      arr.push(4);
+      console.log(arr.length);//4
     });
 
-    it('test 1',function(){
-
+    it('should have a size of 2 when pop a value from the array',function(){
+      
+      arr.pop();
+      console.log(arr.length); //2
     });
 
-    it('test 2',function(){
-
+    it('should remove 3 when use pop in the array',function(){
+      
+      console.log(arr.pop() === 3); //true
     });
 
     
