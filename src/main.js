18a87e7529f15f766cd433f02f8e2690d1865ef5
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-07-16 14:14:42 -0300

    Aula 84. Criando uma calculadora simples Parte 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 module.exports = {
-  sum:function(){},
-  sub:function(){},
-  mult:function(){},
-  div:function(){},
+  sum:function(number1, number2){
+    return number1 + number2;
+  },
+  sub:function(number1, number2){
+    return number1 - number2;
+  },
+  mult:function(number1,number2){
+    return number1 * number2;
+  },
+  div:function(number1, number2){
+    return (number2 === 0) ? 'Não é possível divisão por zero!':number1 / number2;     
+  },
 }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,32 +2,44 @@ var expect = require('chai').expect;
 var calc = require('../src/main.js');
 
 describe('Calc', function () {
-  //smoke test 
+
   describe('Smoke tests',function(){
-    it('should exists the calc lib',function(){
-      expect(calc).to.exist;
+  
+  });
+
+    describe('Sum',function(){
+      it('should return 4 when `sum(2,2)`',function(){
+        expect(calc.sum(2,2)).to.be.equal(4);
+      });
+    });  
+    
+
+  
+    describe('Sub',function(){
+      it('should return 4 when `sub(6,2)`',function(){
+        expect(calc.sub(6,2)).to.be.equal(4);
+      });      
       
-      });
+      it('should return -4 when `sub(6,10)`',function(){
+        expect(calc.sub(6,10)).to.be.equal(-4);
+      });    
+    });      
 
-      it('should exists the method `sum`',function(){
-        expect(calc.sum).to.exist;
-        expect(calc.sum).to.be.a.function;
+    describe('Mult',function(){
+      it('Should return 4 when `mult(2,2)` ',function(){
+        expect(calc.mult(2,2)).to.be.equal(4);
       });
-
-      it('should exists the method `sub`',function(){
-        expect(calc.sub).to.exist;
-        expect(calc.sub).to.be.a.function;
-      });
-
-      it('should exists the method `mult`',function(){
-        expect(calc.mult).to.exist;
-        expect(calc.mult).to.be.a.function;
-      });
-
-      it('should exists the method `div`',function(){
-        expect(calc.div).to.exist;
-        expect(calc.div).to.be.a.function;
-      });
-
     });
-  }); 
+
+    describe('Div',function(){
+      it('Should return 2 when `div(4,2)` ',function(){
+        expect(calc.div(4,2)).to.be.equal(2);
+      });
+
+      it('should return `Não é possível divisão por zero` when divide by 0',function(){
+        expect(calc.div(4,0)).to.be.equal('Não é possível divisão por zero!');
+      });
+    })
+
+  });    
+    
