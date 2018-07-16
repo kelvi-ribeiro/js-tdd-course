430e9370447ff1dd858e3a51151c13ce3dd86ae8
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-07-16 13:38:00 -0300

    Aula 81. Criando uma calculadora simples parte 1

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,50 +1,33 @@
 var expect = require('chai').expect;
+var calc = require('../src/main.js');
 
-describe('Main', function () {
-  var arr; 
-  //Roda uma vez, antes do bloco
-    before(function () {
-      // Pode ser usado para inicar uma conexão no banco
-      // Pode ser usado para criar um conjunto de dados
-  });
-  //Roda uma vez, depois do bloco
-    after(function () {
-      // Pode ser usado para fechar uma conexão no banco
-      // Pode ser usado para apagar um conjunto de dados criado no after
-  });
-    // Roda todas as vezes, antes de CADA bloco
-    beforeEach(function () {
-       arr = [1, 2, 3];
-  });
-    // Roda todas as vezes, depois de CADA bloco
-    afterEach(function () {
+describe('Calc', function () {
+  //smoke test 
+  describe('Smoke tests',function(){
+    it('should exists the calc lib',function(){
+      expect(calc).to.exist;
       
-    });   
-    // Testar tipos ou se existe, é chamado de smoke test
-    it('should be an array',function(){      
-      expect(arr).to.be.a('array');      
-    });
+      });
 
-    it('should return true when pop 3 from the array',function(){      
-      expect(arr.pop() == 3).to.be.true;
-      
-    });
-    
-    
+      it('should exists the method `sum`',function(){
+        expect(calc.sum).to.exist;
+        expect(calc.sum).to.be.a.function;
+      });
 
+      it('should exists the method `sub`',function(){
+        expect(calc.sub).to.exist;
+        expect(calc.sub).to.be.a.function;
+      });
 
-    it('should have a size of 4 when push another value to the array',function(){      
-      arr.push(4);
-      expect(arr).to.have.length(4);      
-    });
+      it('should exists the method `mult`',function(){
+        expect(calc.mult).to.exist;
+        expect(calc.mult).to.be.a.function;
+      });
 
-    it('should have a size of 2 when pop a value from the array',function(){      
-      arr.pop();
-      expect(arr).to.have.length(2);
-    });
+      it('should exists the method `div`',function(){
+        expect(calc.div).to.exist;
+        expect(calc.div).to.be.a.function;
+      });
 
-    it('should remove 3 when use pop in the array',function(){           
-      arr.pop(); //true
-      expect(arr).to.not.include(3)
     });
   }); 
