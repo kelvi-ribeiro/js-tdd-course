425d3416c515a6acf3a9a8fdaffbe4b8856dbb21
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-01 16:54:28 -0300

    Aula 145. Adicionando título

--- projects/tests-with-react/react-full-header/src/Main.js
+++ projects/tests-with-react/react-full-header/src/Main.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const FullHeader = ({ title }) => (
-    <header></header>
-);
+const FullHeader = ({ title }) => {
+    return (
+        <header>
+            {title && <h1>{title}</h1>}
+        </header>
+    )
+}
+
+const propTypes = {
+    title: PropTypes.string
+}
+
+FullHeader.propTypes = propTypes
+
 
 
 
--- projects/tests-with-react/react-full-header/tests/specs/Main.spec.js
+++ projects/tests-with-react/react-full-header/tests/specs/Main.spec.js
@@ -4,8 +4,22 @@ import { shallow } from 'enzyme';
 import FullHeader from '../../src/Main';
 
 describe('FullHeader Hello', () => {
-   it('should have header tag when mount', () => {
-       const wrapper = shallow(<FullHeader/>)
-       expect(wrapper.find('header')).to.have.length(1)
-   })
+    it('should have header tag when mount', () => {
+        const wrapper = shallow(<FullHeader />)
+        expect(wrapper.find('header')).to.have.length(1)
+    })
+
+    it('should have h1 when title passed', () => {
+        const wrapper = shallow(<FullHeader title='TDD' />)
+        expect(wrapper.find('h1')).to.have.length(1)
+    })
+    it('should not have have h1 when title is not passed', () => {
+        const wrapper = shallow(<FullHeader />)
+        expect(wrapper.find('h1')).to.have.length(0)
+    })
+
+    it('should have have h1 with the title passed', () => {
+        const wrapper = shallow(<FullHeader title='TDD' />)
+        expect(wrapper.find('h1').props().children).to.be.equal('TDD')
+    })
 });
