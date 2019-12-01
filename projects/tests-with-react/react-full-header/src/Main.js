49f66aa059b37688ff398f09fe1c8098d6fcdd29
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-12-01 16:42:30 -0300

    Aula 144. Escrevendo primeiro Teste

--- projects/tests-with-react/react-full-header/src/Main.js
+++ projects/tests-with-react/react-full-header/src/Main.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const FullHeader = ({ title }) => (
-    <h1>Hello {title}!</h1>
+    <header></header>
 );
 
-FullHeader.propTypes = {
-    title: PropTypes.string.isRequired,
-};
+
 
 export default FullHeader;
